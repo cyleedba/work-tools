db6a9f3402a6658d0e1acb7f967f1e953d06e6f4
lkk147852 <veryfay40@gmail.com>
2023-11-14 16:13:39 +0800

    Enhance table extraction from SQL dump in script
    This commit updates the shell script to allow users to extract specific tables from a SQL dump file. The script now prompts the user for the dump file name and the table name they wish to extract. It then calculates the start and end lines for the specified table and uses `sed` to extract the relevant portion from the dump file. This enhancement makes the script more versatile and user-friendly for handling large database dumps.

--- import_specific_table.sh
+++ import_specific_table.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# your dump file 
-# if you only want one table dump file not use all of them you can use this command to filter which table you need to use
+# 讓用戶輸入 dump 檔案名和想要提取的表名
+echo "Enter the name of your dump file (e.g., apple_2023-11-06.sql):"
+read dump_file
+echo "Enter the name of the table you want to extract (e.g., core_pay_mpay):"
+read table_name
 
+# 獲取表的起始行
+start_line=$(grep -n "Table structure for table \`$table_name\`" $dump_file | awk -F ':' '{print $1}')
 
-grep -n "Table structure for table" apple_2023-11-06.sql | awk 'NR > 1 { print prev } { prev = $0 }'
+# 獲取表的結束行
+next_table_line=$(grep -n "Table structure for table" $dump_file | awk -F ':' -v start="$start_line" 'NR > 1 && $1 > start {print $1; exit}')
+end_line=$((next_table_line - 1))
 
-
-
-## and get the start line and end line , you can create and new dump file 
-
-sed -n 'start_line,end_line p' dump.sql > after_data.sql
+# 檢查是否找到起始行和結束行
+if [ -n "$start_line" ] && [ -n "$end_line" ]; then
+    # 使用 sed 提取表數據到新文件
+    sed -n "${start_line},${end_line}p" $dump_file > "${table_name}_data.sql"
+    echo "Extracted data for table '$table_name' into ${table_name}_data.sql"
+else
+    echo "Table '$table_name' not found in $dump_file"
+fi
